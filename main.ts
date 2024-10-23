3fc2bf4e11a7a97315c7633be970162a8721d17f
ella-an2 <ella.an@mths.ca>
2024-10-23 10:50:17 -0400

    changed the code

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Ella An
@@ -10,14 +11,13 @@ let numberOne: number
 let numberTwo: number
 
 //setup
-numberTwo = -1
-numberOne = -1
+numberOne = randint(0, 99)
+numberTwo = randint(0, 99)
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 //button a pressed
 input.onButtonPressed(Button.A, function () {
-    numberOne = randint(0, 99)
     basic.showString("#1:" + (numberOne).toString())
     basic.pause(1000)
     basic.showIcon(IconNames.Happy)
@@ -25,7 +25,6 @@ input.onButtonPressed(Button.A, function () {
 
 //button b pressed
 input.onButtonPressed(Button.B, function () {
-    numberTwo = randint(0, 99)
     basic.showString("#2:" + (numberTwo).toString())
     basic.pause(1000)
     basic.showIcon(IconNames.Happy)
